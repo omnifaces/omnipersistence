433dbbd03cb4ca74266c3055aa5eb62c53ab72f6
Jan Beernink <jan.beernink@gmail.com>
2016-07-05 11:22:36 +0200

    Add ability to define setup and teardown handlers for GenericEntityService

--- src/main/java/org/omnifaces/persistence/service/GenericEntityService.java
+++ src/main/java/org/omnifaces/persistence/service/GenericEntityService.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
@@ -52,6 +53,9 @@ public class GenericEntityService {
 	private EntityManager entityManager;
 	private EntityManagerFactory entityManagerFactory;
 
+	private Consumer<EntityManager> setupHandler;
+	private Consumer<EntityManager> teardownHandler;
+
 	public static interface QueryBuilder<T> {
 		Root<?> build(CriteriaBuilder criteriaBuilder, CriteriaQuery<?> criteriaQuery, Class<?> type);
 	}
@@ -66,6 +70,14 @@ public class GenericEntityService {
 
 	public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
 		this.entityManagerFactory = entityManagerFactory;
+	}
+
+	public void setSetupHandler(Consumer<EntityManager> setupHandler) {
+		this.setupHandler = setupHandler;
+	}
+
+	public void setTeardownHandler(Consumer<EntityManager> teardownHandler) {
+		this.teardownHandler = teardownHandler;
 	}
 
 	public BaseEntity<? extends Number> find(Class<BaseEntity<? extends Number>> type, Number id) {
@@ -137,173 +149,195 @@ public class GenericEntityService {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private <T> PartialResultList<T> getAllPagedAndSorted(Class<T> returnType, QueryBuilder<?> queryBuilder, Map<String, Object> parameters, SortFilterPage sortFilterPage, boolean getCount, boolean isSorted, boolean isCached) {
 
-		// Create the two standard JPA objects used for criteria query construction
-		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-		CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(returnType);
+		if (setupHandler != null) {
+			setupHandler.accept(entityManager);
+		}
 
-		// Obtain main query from the passed-in builder
-		Root<?> root = 	queryBuilder.build(criteriaBuilder, criteriaQuery, returnType);
+		try {
+			// Create the two standard JPA objects used for criteria query construction
+			CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+			CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(returnType);
 
-		// Add sorting to query if necessary
-		if (!isSorted && !isEmpty(sortFilterPage.getSortField())) {
-			criteriaQuery.orderBy(
-				"ASCENDING".equals(sortFilterPage.getSortOrder())?
-					criteriaBuilder.asc(root.get(sortFilterPage.getSortField())) :
-					criteriaBuilder.desc(root.get(sortFilterPage.getSortField()))
+			// Obtain main query from the passed-in builder
+			Root<?> root = queryBuilder.build(criteriaBuilder, criteriaQuery, returnType);
+
+			// Add sorting to query if necessary
+			if (!isSorted && !isEmpty(sortFilterPage.getSortField())) {
+				criteriaQuery.orderBy(
+						"ASCENDING".equals(sortFilterPage.getSortOrder()) ?
+						criteriaBuilder.asc(root.get(sortFilterPage.getSortField())) :
+						criteriaBuilder.desc(root.get(sortFilterPage.getSortField()))
+				);
+			}
+
+			List<Predicate> searchPredicates = new ArrayList<>();
+			List<Predicate> exactPredicates = new ArrayList<>();
+
+			// Add filtering to query
+			sortFilterPage.getFilterValues().entrySet().forEach(
+					e -> {
+						String key = e.getKey() + "Search";
+						String value = e.getValue().toString();
+						Class<?> type = root.get(e.getKey()).getJavaType();
+
+						if (type.isEnum()) {
+							try {
+								Enum enumValue = Enum.valueOf((Class<Enum>) type, value);
+								exactPredicates.add(criteriaBuilder.equal(root.get(e.getKey()),
+								                                          criteriaBuilder.parameter(type, key)));
+								parameters.put(key, enumValue);
+							}
+							catch (IllegalArgumentException ignore) {
+								//
+							}
+						}
+						else if (type.isAssignableFrom(Boolean.class)) {
+							exactPredicates.add(
+									criteriaBuilder.equal(root.get(e.getKey()), criteriaBuilder.parameter(type, key)));
+							parameters.put(key, Boolean.valueOf(value));
+						}
+						else if (type.isAssignableFrom(Long.class)) {
+							if (isOneOf(value, "true", "false")) {
+								Path<Long> path = root.get(e.getKey());
+								ParameterExpression<Long> parameter = criteriaBuilder.parameter(Long.class, key);
+								exactPredicates.add("true".equals(value) ? criteriaBuilder.gt(path, parameter) :
+								                    criteriaBuilder.le(path, parameter));
+								parameters.put(key, 0L);
+							}
+						}
+						else if (!sortFilterPage.getFilterableFields().contains(e.getKey())) {
+							exactPredicates.add(
+									criteriaBuilder.equal(root.get(e.getKey()), criteriaBuilder.parameter(type, key)));
+							parameters.put(key, value);
+						}
+
+						if (!parameters.containsKey(key)) {
+							searchPredicates.add(criteriaBuilder.like(criteriaBuilder.lower(
+									criteriaBuilder.function("str", String.class, root.get(e.getKey()))),
+							                                          criteriaBuilder.parameter(String.class, key)));
+							parameters.put(key, "%" + value.toLowerCase() + "%");
+						}
+					}
 			);
-		}
 
-		List<Predicate> searchPredicates = new ArrayList<>();
-		List<Predicate> exactPredicates = new ArrayList<>();
+			Predicate newRestrictions = null;
 
-		// Add filtering to query
-		sortFilterPage.getFilterValues().entrySet().forEach(
-			e -> {
-				String key = e.getKey() + "Search";
-				String value = e.getValue().toString();
-				Class<?> type = root.get(e.getKey()).getJavaType();
+			if (!searchPredicates.isEmpty()) {
+				newRestrictions = sortFilterPage.isFilterWithAND() ?
+				                  criteriaBuilder.and(searchPredicates.toArray(PREDICATE_ARRAY)) :
+				                  criteriaBuilder.or(searchPredicates.toArray(PREDICATE_ARRAY));
+			}
 
-				if (type.isEnum()) {
-					try {
-						Enum enumValue = Enum.valueOf((Class<Enum>) type, value);
-						exactPredicates.add(criteriaBuilder.equal(root.get(e.getKey()), criteriaBuilder.parameter(type, key)));
-						parameters.put(key, enumValue);
+			if (!exactPredicates.isEmpty()) {
+				Predicate exactRestrictions = criteriaBuilder.and(exactPredicates.toArray(PREDICATE_ARRAY));
+				newRestrictions = newRestrictions != null ? criteriaBuilder.and(newRestrictions, exactRestrictions) :
+				                  exactRestrictions;
+			}
+
+			if (newRestrictions != null) {
+				Predicate originalRestrictions = criteriaQuery.getRestriction();
+
+				if (originalRestrictions != null) {
+					criteriaQuery.where(criteriaBuilder.and(originalRestrictions, newRestrictions));
+				}
+				else {
+					criteriaQuery.where(newRestrictions);
+				}
+			}
+
+			// Create the "actual" JPA query from the above constructed criteria query
+			// and add paging
+			TypedQuery<T> typedQuery = entityManager
+					.createQuery(criteriaQuery)
+					.setFirstResult(sortFilterPage.getOffset())
+					.setMaxResults(sortFilterPage.getLimit())
+					.setHint("org.hibernate.cacheable", isCached ? "true" : "false")
+					// TODO: Not just a global region but per query
+					.setHint("org.hibernate.cacheRegion", "genericEntityServiceRegion");
+
+			// Set parameters on the query. This includes both the provided parameters and the
+			// ones for filtering that we generated here.
+			parameters.entrySet().forEach(
+					e -> typedQuery.setParameter(e.getKey(), e.getValue())
+			);
+
+			// Execute query
+			List<T> entities = typedQuery.getResultList();
+
+			// Troublesome Hibernate specific code to get total number of the results for the above query
+			// without the paging
+			Long count = -1l;
+			if (getCount) {
+
+				// Reset order by since count queries do not need sorting, it causes high memory consumption
+				// or even temporary file generation in the database if the result set is rather large.
+				javax.persistence.Query countQuery = entityManager.createQuery(criteriaQuery.orderBy());
+
+				for (Map.Entry<String, Object> parameterEntry : parameters.entrySet()) {
+					countQuery.setParameter(parameterEntry.getKey(), parameterEntry.getValue());
+				}
+
+				QueryTranslator translator = translateFromQuery(countQuery);
+				javax.persistence.Query nativeQuery = entityManager.createNativeQuery(
+						"select count(*) from (" +
+						translator.getSQLString() +
+						") x"
+				); // not cacheable:  https://hibernate.atlassian.net/browse/HHH-9111 http://stackoverflow.com/q/25789176
+
+				ParameterTranslations parameterTranslations = translator.getParameterTranslations();
+
+				Query query = typedQuery.unwrap(Query.class);
+				Map<String, TypedValue> namedParams = null;
+				SessionImplementor session = null;
+				try {
+
+					// Yes, the following code is dreadful...
+
+					Method method = AbstractQueryImpl.class.getDeclaredMethod("getNamedParams");
+					method.setAccessible(true);
+					Object map = method.invoke(query);
+					namedParams = (Map<String, TypedValue>) map;
+
+
+					method = AbstractQueryImpl.class.getDeclaredMethod("getSession");
+					method.setAccessible(true);
+					Object sessionObject = method.invoke(query);
+					session = (SessionImplementor) sessionObject;
+				}
+				catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
+					e1.printStackTrace();
+				}
+
+				CapturingStatement capturingStatement = new CapturingStatement();
+				for (Parameter<?> parameter : countQuery.getParameters()) {
+					for (int position : parameterTranslations.getNamedParameterSqlLocations(parameter.getName())) {
+
+						TypedValue typedValue = namedParams.get(parameter.getName());
+
+						try {
+							// Convert the parameter value
+							typedValue.getType()
+							          .nullSafeSet(capturingStatement, typedValue.getValue(), position + 1, session);
+						}
+						catch (HibernateException | SQLException e1) {
+							e1.printStackTrace();
+						}
+
+
+						nativeQuery.setParameter(position + 1, capturingStatement.getObject());
 					}
-					catch (IllegalArgumentException ignore) {
-						//
-					}
-				}
-				else if (type.isAssignableFrom(Boolean.class)) {
-					exactPredicates.add(criteriaBuilder.equal(root.get(e.getKey()), criteriaBuilder.parameter(type, key)));
-					parameters.put(key, Boolean.valueOf(value));
-				}
-				else if (type.isAssignableFrom(Long.class)) {
-					if (isOneOf(value, "true", "false")) {
-						Path<Long> path = root.get(e.getKey());
-						ParameterExpression<Long> parameter = criteriaBuilder.parameter(Long.class, key);
-						exactPredicates.add("true".equals(value) ? criteriaBuilder.gt(path, parameter) : criteriaBuilder.le(path, parameter));
-						parameters.put(key, 0L);
-					}
-				}
-				else if (!sortFilterPage.getFilterableFields().contains(e.getKey())) {
-					exactPredicates.add(criteriaBuilder.equal(root.get(e.getKey()), criteriaBuilder.parameter(type, key)));
-					parameters.put(key, value);
 				}
 
-				if (!parameters.containsKey(key)) {
-					searchPredicates.add(criteriaBuilder.like(criteriaBuilder.lower(criteriaBuilder.function("str", String.class, root.get(e.getKey()))), criteriaBuilder.parameter(String.class, key)));
-					parameters.put(key, "%" + value.toLowerCase() + "%");
-				}
+				count = ((Number) nativeQuery.getSingleResult()).longValue();
 			}
-		);
 
-		Predicate newRestrictions = null;
-
-		if (!searchPredicates.isEmpty()) {
-			newRestrictions = sortFilterPage.isFilterWithAND() ?
-				criteriaBuilder.and(searchPredicates.toArray(PREDICATE_ARRAY)) :
-				criteriaBuilder.or(searchPredicates.toArray(PREDICATE_ARRAY));
+			return new PartialResultList<>(entities, sortFilterPage.getOffset(), count.intValue());
 		}
-
-		if (!exactPredicates.isEmpty()) {
-			Predicate exactRestrictions = criteriaBuilder.and(exactPredicates.toArray(PREDICATE_ARRAY));
-			newRestrictions = newRestrictions != null ? criteriaBuilder.and(newRestrictions, exactRestrictions) : exactRestrictions;
-		}
-
-		if (newRestrictions != null) {
-			Predicate originalRestrictions = criteriaQuery.getRestriction();
-
-			if (originalRestrictions != null) {
-				criteriaQuery.where(criteriaBuilder.and(originalRestrictions, newRestrictions));
-			} else {
-				criteriaQuery.where(newRestrictions);
+		finally {
+			if (teardownHandler != null) {
+				teardownHandler.accept(entityManager);
 			}
 		}
-
-		// Create the "actual" JPA query from the above constructed criteria query
-		// and add paging
-		TypedQuery<T> typedQuery = entityManager
-			.createQuery(criteriaQuery)
-			.setFirstResult(sortFilterPage.getOffset())
-			.setMaxResults(sortFilterPage.getLimit())
-			.setHint("org.hibernate.cacheable", isCached ? "true" : "false")
-			// TODO: Not just a global region but per query
-			.setHint("org.hibernate.cacheRegion", "genericEntityServiceRegion");
-
-		// Set parameters on the query. This includes both the provided parameters and the
-		// ones for filtering that we generated here.
-		parameters.entrySet().forEach(
-			e -> typedQuery.setParameter(e.getKey(), e.getValue())
-		);
-
-		// Execute query
-		List<T> entities = typedQuery.getResultList();
-
-		// Troublesome Hibernate specific code to get total number of the results for the above query
-		// without the paging
-		Long count = -1l;
-		if (getCount) {
-
-			// Reset order by since count queries do not need sorting, it causes high memory consumption
-			// or even temporary file generation in the database if the result set is rather large.
-			javax.persistence.Query countQuery = entityManager.createQuery(criteriaQuery.orderBy());
-
-			for (Map.Entry<String, Object> parameterEntry : parameters.entrySet()) {
-				countQuery.setParameter(parameterEntry.getKey(), parameterEntry.getValue());
-			}
-
-			QueryTranslator translator = translateFromQuery(countQuery);
-			javax.persistence.Query nativeQuery = entityManager.createNativeQuery(
-				"select count(*) from (" +
-					translator.getSQLString() +
-				") x"
-			); // not cacheable:  https://hibernate.atlassian.net/browse/HHH-9111 http://stackoverflow.com/q/25789176
-
-			ParameterTranslations parameterTranslations = translator.getParameterTranslations();
-
-			Query query = typedQuery.unwrap(Query.class);
-			Map<String,TypedValue> namedParams = null;
-			SessionImplementor session = null;
-			try {
-
-				// Yes, the following code is dreadful...
-
-				Method method = AbstractQueryImpl.class.getDeclaredMethod("getNamedParams");
-				method.setAccessible(true);
-				Object map = method.invoke(query);
-				namedParams = (Map<String, TypedValue>) map;
-
-
-				method = AbstractQueryImpl.class.getDeclaredMethod("getSession");
-				method.setAccessible(true);
-				Object sessionObject = method.invoke(query);
-				session = (SessionImplementor) sessionObject;
-			} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
-				e1.printStackTrace();
-			}
-
-			CapturingStatement capturingStatement = new CapturingStatement();
-			for (Parameter<?> parameter : countQuery.getParameters()) {
-				for (int position : parameterTranslations.getNamedParameterSqlLocations(parameter.getName())) {
-
-					TypedValue typedValue = namedParams.get(parameter.getName());
-
-					try {
-						// Convert the parameter value
-						typedValue.getType().nullSafeSet(capturingStatement, typedValue.getValue(), position + 1, session);
-					} catch (HibernateException | SQLException e1) {
-						e1.printStackTrace();
-					}
-
-
-					nativeQuery.setParameter(position + 1, capturingStatement.getObject());
-				}
-			}
-
-			count = ((Number) nativeQuery.getSingleResult()).longValue();
-		}
-
-		return new PartialResultList<>(entities, sortFilterPage.getOffset(), count.intValue());
 	}
 
 }
