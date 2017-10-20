ad9d9e81aec5036bfe0cb8c7a2b859d2007b5fc2
Bauke Scholtz <balusc@gmail.com>
2017-10-20 21:51:54 +0200

    Cleanup JPA utility class

--- src/main/java/org/omnifaces/persistence/JPA.java
+++ src/main/java/org/omnifaces/persistence/JPA.java
@@ -12,10 +12,10 @@
  */
 package org.omnifaces.persistence;
 
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 import static org.omnifaces.persistence.Database.POSTGRESQL;
 import static org.omnifaces.persistence.Provider.HIBERNATE;
-import static org.omnifaces.utils.reflect.Reflections.findClass;
 import static org.omnifaces.utils.stream.Collectors.toMap;
 import static org.omnifaces.utils.stream.Streams.stream;
 
@@ -26,19 +26,16 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.enterprise.inject.Typed;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CollectionJoin;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Join;
-import javax.persistence.criteria.ListJoin;
-import javax.persistence.criteria.MapJoin;
 import javax.persistence.criteria.Root;
-import javax.persistence.criteria.SetJoin;
 import javax.persistence.metamodel.Attribute;
 import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.EntityType;
@@ -49,121 +46,182 @@ import javax.persistence.metamodel.PluralAttribute;
 import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 
+/**
+ * JPA utilities.
+ */
+@Typed
 public final class JPA {
+
+	// Public constants -------------------------------------------------------------------------------------------------------------------
 
 	public static final String LOAD_GRAPH_HINT_KEY = "javax.persistence.loadgraph";
 	public static final String FETCH_GRAPH_HINT_KEY = "javax.persistence.fetchgraph";
 	public static final String CACHE_RETRIEVE_MODE_HINT_KEY = "javax.persistence.cache.retrieveMode";
 
-	private static final Optional<Class<Object>> HIBERNATE_PROXY_CLASS = findClass("org.hibernate.proxy.HibernateProxy");
+
+	// Constructors -----------------------------------------------------------------------------------------------------------------------
 
 	private JPA() {
-		//
+		throw new AssertionError();
 	}
 
-	public static <T> T getOptionalSingleResult(TypedQuery<T> query) {
+
+	// Query utils ------------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Returns single result of given typed query as {@link Optional}.
+	 * @param <T> The generic result type.
+	 * @param typedQuery The involved typed query.
+	 * @return Single result of given typed query as {@link Optional}.
+	 */
+	public static <T> Optional<T> getOptionalSingleResult(TypedQuery<T> typedQuery) {
+		return ofNullable(getSingleResultOrNull(typedQuery));
+	}
+
+	/**
+	 * Returns single result of given query as {@link Optional}.
+	 * @param <T> The expected result type.
+	 * @param query The involved query.
+	 * @param resultType The expected result type.
+	 * @return Single result of given query as {@link Optional}.
+	 * @throws ClassCastException When <code>T</code> is of wrong type.
+	 */
+	public static <T> Optional<T> getOptionalSingleResult(Query query, Class<T> resultType) {
+		return ofNullable(getSingleResultOrNull(query, resultType));
+	}
+
+	/**
+	 * Returns single result of given typed query, or <code>null</code> if there is none.
+	 * @param <T> The generic result type.
+	 * @param typedQuery The involved typed query.
+	 * @return Single result of given typed query, or <code>null</code> if there is none.
+	 */
+	public static <T> T getSingleResultOrNull(TypedQuery<T> typedQuery) {
+		return getSingleResultOrNull(typedQuery, null);
+	}
+
+	/**
+	 * Returns single result of given query, or <code>null</code> if there is none.
+	 * @param <T> The expected result type.
+	 * @param query The involved query.
+	 * @param resultType The expected result type.
+	 * @return Single result of given query, or <code>null</code> if there is none.
+	 * @throws ClassCastException When <code>T</code> is of wrong type.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T getSingleResultOrNull(Query query, Class<T> resultType) {
 		try {
 			query.setMaxResults(1);
-			return query.getSingleResult();
+			Object singleResult = query.getSingleResult();
+			return resultType == null ? (T) singleResult : resultType.cast(singleResult);
 		}
 		catch (NoResultException e) {
 			return null;
 		}
 	}
 
-	public static <T> Optional<T> getOptional(TypedQuery<T> query) {
-		try {
-			query.setMaxResults(1);
-			return Optional.of(query.getSingleResult());
-		}
-		catch (NoResultException e) {
-			return Optional.empty();
-		}
+	/**
+	 * Returns the result list of given typed query as a map mapped by the given key mapper.
+	 * @param <K> The generic map key type.
+	 * @param <T> The generic result type, also map value type.
+	 * @param typedQuery The involved typed query.
+	 * @param keyMapper The key mapper.
+	 * @return The result list of given typed query as a map mapped by the given key mapper.
+	 */
+	public static <K, T> Map<K, T> getResultMap(TypedQuery<T> typedQuery, Function<? super T, ? extends K> keyMapper) {
+		return typedQuery.getResultList().stream().collect(toMap(keyMapper));
 	}
 
-	public static <K, T> Map<K, T> getResultMap(TypedQuery<T> query, Function<? super T, ? extends K> keyMapper) {
-		return query.getResultList()
-					.stream()
-					.collect(toMap(keyMapper));
-	}
-
+	/**
+	 * Returns the result list of given typed query as a map mapped by the given key and value mappers.
+	 * @param <K> The generic map key type.
+	 * @param <T> The generic result type.
+	 * @param <V> The generic map value type.
+	 * @param query The involved typed query.
+	 * @param keyMapper The key mapper.
+	 * @param valueMapper The value mapper.
+	 * @return The result list of given typed query as a map mapped by the given key and value mappers.
+	 */
 	public static <K, T, V> Map<K, V> getResultMap(TypedQuery<T> query, Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends V> valueMapper) {
-		return query.getResultList()
-					.stream()
-					.collect(Collectors.toMap(keyMapper, valueMapper));
+		return query.getResultList().stream().collect(Collectors.toMap(keyMapper, valueMapper));
 	}
 
-	public static <T> T getOptionalSingleResult(Query query, Class<T> type) {
-		try {
-			query.setMaxResults(1);
-			return type.cast(query.getSingleResult());
-		}
-		catch (NoResultException e) {
-			return null;
-		}
-	}
 
-	public static <T, I> Long getForeignKeyReferences(Class<T> entityClass, Class<I> idType, I entityId, EntityManager entityManager) {
+	// Entity utils -----------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Returns count of all foreign key references to entity of given entity type with given ID of given identifier type.
+	 * This is particularly useful in case you intend to check if the given entity is orphaned in that perspective.
+	 * @param <T> The generic result type.
+	 * @param <I> The generic identifier type.
+	 * @param entityManager The involved entity manager.
+	 * @param entityType Entity type.
+	 * @param identifierType Identifier type.
+	 * @param id Entity ID.
+	 * @return Count of all foreign key references to entity of given entity type with given ID of given identifier type.
+	 */
+	public static <T, I> Long countForeignKeyReferences(EntityManager entityManager, Class<T> entityType, Class<I> identifierType, I id) {
 		Metamodel metamodel = entityManager.getMetamodel();
-		EntityType<T> entityType = metamodel.entity(entityClass);
-		SingularAttribute<? super T, I> idAttribute = entityType.getId(idType);
-
-		return metamodel.getEntities()
-						.stream()
-						.flatMap(entity -> getAttributesOfType(entity, entityClass))
-						.distinct()
-						.mapToLong(attribute -> countReferencesTo(entityManager, attribute, idAttribute, entityId))
-						.sum();
+		SingularAttribute<? super T, I> idAttribute = metamodel.entity(entityType).getId(identifierType);
+		return metamodel.getEntities().stream()
+			.flatMap(entity -> getAttributesOfType(entity, entityType))
+			.distinct()
+			.mapToLong(attribute -> countReferencesTo(entityManager, attribute, idAttribute, id))
+			.sum();
 	}
 
-	private static <T, E> Stream<Attribute<?, ?>> getAttributesOfType(EntityType<T> entityType, Class<E> entityClass) {
-		return entityType.getAttributes()
-						 .stream()
-						 .filter(attribute -> {
-							 if (attribute instanceof PluralAttribute) {
-								 return entityClass.equals(((PluralAttribute<?, ?, ?>) attribute).getElementType().getJavaType());
-							 }
+	private static <E, T> Stream<Attribute<?, ?>> getAttributesOfType(EntityType<E> entity, Class<T> entityType) {
+		return entity.getAttributes().stream()
+			.filter(attribute -> entityType.equals(getJavaType(attribute)))
+			.map(attribute -> attribute);
+	}
 
-							 return entityClass.equals(attribute.getJavaType());
-						 })
-						 .map(attribute -> attribute);
+	private static <E> Class<?> getJavaType(Attribute<? super E, ?> attribute) {
+		return (attribute instanceof PluralAttribute)
+			? ((PluralAttribute<?, ?, ?>) attribute).getElementType().getJavaType()
+			: attribute.getJavaType();
 	}
 
 	@SuppressWarnings("unchecked")
-	private static <R, T, I> Long countReferencesTo(EntityManager entityManager, Attribute<R, ?> attribute, SingularAttribute<? super T, I> idAttribute, I id) {
+	private static <E, T, I> Long countReferencesTo(EntityManager entityManager, Attribute<E, ?> attribute, SingularAttribute<? super T, I> idAttribute, I id) {
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 		CriteriaQuery<Long> query = criteriaBuilder.createQuery(Long.class);
-		Root<R> root = query.from(attribute.getDeclaringType().getJavaType());
+		Root<E> root = query.from(attribute.getDeclaringType().getJavaType());
+		Join<E, T> join;
 
 		if (attribute instanceof SingularAttribute) {
-			Join<R, T> relation = root.join((SingularAttribute<R, T>) attribute);
-			query.where(criteriaBuilder.equal(relation.get(idAttribute), id));
+			join = root.join((SingularAttribute<E, T>) attribute);
 		}
 		else if (attribute instanceof ListAttribute) {
-			ListJoin<R, T> relation = root.join((ListAttribute<R, T>) attribute);
-			query.where(criteriaBuilder.equal(relation.get(idAttribute), id));
+			join = root.join((ListAttribute<E, T>) attribute);
 		}
 		else if (attribute instanceof SetAttribute) {
-			SetJoin<R, T> relation = root.join((SetAttribute<R, T>) attribute);
-			query.where(criteriaBuilder.equal(relation.get(idAttribute), id));
+			join = root.join((SetAttribute<E, T>) attribute);
 		}
 		else if (attribute instanceof MapAttribute) {
-			MapJoin<R, ?, T> relation = root.join((MapAttribute<R, ?, T>) attribute);
-			query.where(criteriaBuilder.equal(relation.get(idAttribute), id));
+			join = root.join((MapAttribute<E, ?, T>) attribute);
 		}
 		else if (attribute instanceof CollectionAttribute) {
-			CollectionJoin<R, T> relation = root.join((CollectionAttribute<R, T>) attribute);
-			query.where(criteriaBuilder.equal(relation.get(idAttribute), id));
+			join = root.join((CollectionAttribute<E, T>) attribute);
 		}
 		else {
-			// Unknown attribute type, just return 0L.
-			return 0L;
+			return 0L; // Unknown attribute type, just return 0.
 		}
 
-		query.select(criteriaBuilder.count(root));
+		query.select(criteriaBuilder.count(root)).where(criteriaBuilder.equal(join.get(idAttribute), id));
 		return entityManager.createQuery(query).getSingleResult();
 	}
 
+
+	// Criteria utils ---------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Returns a SQL CONCAT(...) of given expressions or strings.
+	 * @param builder The involved criteria builder.
+	 * @param expressionsOrStrings Expressions or Strings.
+	 * @return A SQL CONCAT(...) of given expressions or strings.
+	 * @throws IllegalArgumentException When there are less than 2 expressions or strings. There's no point of concat then.
+	 */
 	@SuppressWarnings("unchecked")
 	public static Expression<String> concat(CriteriaBuilder builder, Object... expressionsOrStrings) {
 		if (expressionsOrStrings.length < 2) {
@@ -182,6 +240,13 @@ public final class JPA {
 		return builder.function("CONCAT", String.class, expressions.toArray(new Expression[expressions.size()]));
 	}
 
+	/**
+	 * Returns a new expression wherein given expression is cast as String.
+	 * This covers known problems with certain providers and/or databases.
+	 * @param builder The involved criteria builder.
+	 * @param expression Expression to be cast as String.
+	 * @return A new expression wherein given expression is cast as String.
+	 */
 	@SuppressWarnings("unchecked")
 	public static Expression<String> castAsString(CriteriaBuilder builder, Expression<?> expression) {
 		boolean numeric = Number.class.isAssignableFrom(expression.getJavaType());
