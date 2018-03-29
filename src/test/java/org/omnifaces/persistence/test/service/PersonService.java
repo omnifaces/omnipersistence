430985ebe3e22a59cba627612dab06e7b8caca3c
BalusC <balusc@gmail.com>
2018-03-29 20:55:58 +0200

    Fix old address.string property which was removed

--- src/test/java/org/omnifaces/persistence/test/service/PersonService.java
+++ src/test/java/org/omnifaces/persistence/test/service/PersonService.java
@@ -14,7 +14,6 @@ package org.omnifaces.persistence.test.service;
 
 import static org.omnifaces.persistence.Database.POSTGRESQL;
 import static org.omnifaces.persistence.JPA.concat;
-import static org.omnifaces.persistence.Provider.HIBERNATE;
 
 import java.util.LinkedHashMap;
 
@@ -60,13 +59,7 @@ public class PersonService extends BaseEntityService<Long, Person> {
 			LinkedHashMap<Getter<PersonCard>, Expression<?>> mapping = new LinkedHashMap<>();
 			mapping.put(PersonCard::getId, person.get("id"));
 			mapping.put(PersonCard::getEmail, person.get("email"));
-
-			if (getProvider() == HIBERNATE) {
-				mapping.put(PersonCard::getAddressString, personAddress.get("string")); // address.string uses Hibernate specific @Formula, so no need for manual concat().
-			}
-			else {
-				mapping.put(PersonCard::getAddressString, concat(builder, personAddress.get("street"), " ", personAddress.get("houseNumber"), ", ", personAddress.get("postcode"), " ", personAddress.get("city"), ", ", personAddress.get("country")));
-			}
+			mapping.put(PersonCard::getAddressString, concat(builder, personAddress.get("street"), " ", personAddress.get("houseNumber"), ", ", personAddress.get("postcode"), " ", personAddress.get("city"), ", ", personAddress.get("country")));
 
 			if (getDatabase() == POSTGRESQL) { // Doesn't hurt on other DBs but makes query unnecessarily bloated.
 				query.groupBy(personAddress);
